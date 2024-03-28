526d2322c4de42e0a6347641293b920084b19578
asmaa905 <asmaa_abdelnaser38@yahoo.com>
2024-03-28 08:52:11 +0200

    upload-first-commit

---
+++ assets/js/main.js
@@ -0,0 +1,70 @@
+/*===== MENU SHOW =====*/
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
+
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show");
+    });
+  }
+};
+showMenu("nav-toggle", "nav-menu");
+
+/*===== ACTIVE AND REMOVE MENU =====*/
+const navLinks = document.querySelectorAll(".nav-link");
+const sections = document.querySelectorAll(".section");
+
+window.addEventListener("scroll", () => {
+  let current = '';
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if (scrollY >= sectionTop - 390) {
+      current = section.getAttribute('id');
+    }
+  })
+
+  navLinks.forEach(link => {
+    link.classList.remove('active');
+    if (link.classList.contains(current)) {
+      link.classList.add('active');
+    }
+  })
+})
+
+//   /*Remove menu mobile*/
+const navMenu = document.getElementById("nav-menu");
+//   navMenu.classList.remove("show");
+// }
+navLinks.forEach((n) => n.addEventListener("click", () => { navMenu.classList.remove("show") }));
+
+/*===== COPY Email =====*/
+const copy = document.getElementById("copy");
+copy.addEventListener("click", () => {
+  navigator.clipboard.writeText("aa4080@fayoum.edu.eg");
+  copy.innerHTML = "copied";
+  setTimeout(() => {
+    copy.innerHTML = null;
+  }, 1000);
+});
+
+  function myFunction(){
+    var element = document.body;
+    element.classList.toggle("dark-mode")
+  }
+
+
+  var messageArr = ["Frontend Developer", "FullStack Developer", "Backend Developer"];
+  var textPosition = 0;
+  var speed = 200;
+
+  typewriter = () => {
+    document.querySelector("#jobTitle").innerHTML = messageArr[0].substring(0, textPosition)  ;
+    if(textPosition ++  != messageArr[0].length)
+        setTimeout(typewriter, speed)
+  }
+
+
+  window.addEventListener("load" , typewriter);
+
